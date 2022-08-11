8d9258bcb4fab125f583d9f3b5c39ea44236ab58
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-11 16:44:47 +0300

    dive deeper in dynamic routes and created complex dynamic paths

---
+++ pages/clients/[id]/[clientprojectId].js
@@ -0,0 +1,7 @@
+const SelectedClientProjectPage = () => {
+    return <div>
+        <h1>The Project Page for a Spicific Project for a Selected Client</h1>
+    </div>
+}
+
+export default SelectedClientProjectPage
---
+++ pages/clients/index.js
@@ -0,0 +1,5 @@
+const ClientsPage = () => {
+ return <div><h1>The Clietns Page</h1></div>
+}
+
+export default ClientsPage;
